d6b35d23014c7ef0b2e190479824c8f1d5529323
Chingiz Ahmadov <chingiz511@gmail.com>
2021-02-25 22:07:07 +0400

    First Commit

---
+++ src/Library/LibraryAPI/Entities/Movie.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.Entities
+{
+    public class Movie
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+    }
+}
